97230d40d2c2ceb8d965dbbf7110dbdd9115fb3b
cedric3347 <ccterry04@gmail.com>
2025-05-15 15:54:39 -0400

    markdown_block with tests added

---
+++ src/markdown_block.py
@@ -0,0 +1,13 @@
+
+
+
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    clean_blocks = []
+    
+    for block in blocks:
+        stripped = block.strip()
+        if stripped:
+            clean_blocks.append(stripped)
+    
+    return clean_blocks
